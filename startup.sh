b99925e20697b8f3649cff07a48ee12f5fb10092
XiTang <tang128xi@gmail.com>
2022-10-20 10:35:32 +0800

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,31 @@
+echo "==== start be.0 ===="
+cd be.0
+./bin/start_be.sh --daemon
+echo "==== start be.1 ===="
+cd ../be.1
+./bin/start_be.sh --daemon
+echo "==== start be.2 ===="
+cd ../be.2
+./bin/start_be.sh --daemon
+echo "==== start be.3 ===="
+cd ../be.3
+./bin/start_be.sh --daemon
+echo "==== start be.4 ===="
+cd ../be.4
+./bin/start_be.sh --daemon
+echo "==== start be.5 ===="
+cd ../be.5
+./bin/start_be.sh --daemon
+echo "==== start be.6 ===="
+cd ../be.6
+./bin/start_be.sh --daemon
+echo "==== start be.7 ===="
+cd ../be.7
+./bin/start_be.sh --daemon
+echo "==== start be.8 ===="
+cd ../be.8
+./bin/start_be.sh --daemon
+echo "==== start be.9 ===="
+cd ../be.9
+./bin/start_be.sh --daemon
+cd ../
---
+++ stop.sh
@@ -0,0 +1,31 @@
+echo "==== stop be.0 ===="
+cd be.0
+./bin/stop_be.sh
+echo "==== stop be.1 ===="
+cd ../be.1
+./bin/stop_be.sh
+echo "==== stop be.2 ===="
+cd ../be.2
+./bin/stop_be.sh
+echo "==== stop be.3 ===="
+cd ../be.3
+./bin/stop_be.sh
+echo "==== stop be.4 ===="
+cd ../be.4
+./bin/stop_be.sh
+echo "==== stop be.5 ===="
+cd ../be.5
+./bin/stop_be.sh
+echo "==== stop be.6 ===="
+cd ../be.6
+./bin/stop_be.sh
+echo "==== stop be.7 ===="
+cd ../be.7
+./bin/stop_be.sh
+echo "==== stop be.8 ===="
+cd ../be.8
+./bin/stop_be.sh
+echo "==== stop be.9 ===="
+cd ../be.9
+./bin/stop_be.sh
+cd ../
